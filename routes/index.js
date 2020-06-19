120f724b86639972ae2ff5d08b190a0d14db08c0
Jack Clarke <jclar162@uottawa.ca>
2020-06-18 21:07:54 -0400

    made a home page for the website

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var imageURLs = [
+  "images/external-content.duckduckgo.com.jpeg"
+, "images/jack.jpeg"
+, "images/jack2.jpeg",
+"images/jackcard.jpeg",
+"images/jacklayton.jpeg",
+"images/jackskull.jpeg"
+];
+
+function getImageTag() {
+  var img = 'img(src=\"';
+  var randomIndex = Math.floor(Math.random() * imageURLs.length);
+  img += imageURLs[randomIndex];
+  img += '\" alt=\"a picture of Jack\" class=\"profilepic\")';
+  return img;
+ }
+
+ imgid = getImageTag()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { profilePic:getImageTag()});
 });
 
 module.exports = router;
